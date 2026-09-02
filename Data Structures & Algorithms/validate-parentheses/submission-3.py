730eafef07c3e1797b9d0e76ad51d17440a417ab
Guy First <guyfirst19@gmail.com>
2026-09-02 15:32:39 +0300

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        #define a hashmap between opening/closing
+        hashmap = {')':'(', '}':'{', ']':'['}
+        open_ = ['(', '[', '{']
+        stack = []
+
+        for brac in s:
+            if brac in open_:
+                stack.append(brac)
+            elif brac in hashmap:
+                if not stack or stack[-1] != hashmap[brac]:
+                        return False
+                stack.pop()
+            
+        
+
+        return len(stack) == 0
+
+        
